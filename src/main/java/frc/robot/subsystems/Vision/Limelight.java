a45fefdf4627f8bb26aa6d21f2b950628d6790ce
nav-util <navka.si5787@vusd.us>
2025-12-10 07:22:40 -0800

    created vision creation system + limelight class

--- src/main/java/frc/robot/subsystems/Vision/Limelight.java
+++ src/main/java/frc/robot/subsystems/Vision/Limelight.java
@@ -1,9 +1,21 @@
 package frc.robot.subsystems.Vision;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import frc.robot.RobotContainer;
 // import edu.wpi.first.math.geometry.Pose2d;
 // import frc.robot.subsystems.Vision.LimelightHelpers;
-import frc.robot.subsystems.Vision.LimelightHelpers.PoseEstimate;
 
 public class Limelight {
-   PoseEstimate pose = LimelightHelpers.getBotPoseEstimate_wpiBlue("limelight");
+   public static Limelight mInstance = null;
+   public static Limelight getInstance() {
+       if(mInstance==null){
+           mInstance = new Limelight();
+       }
+       return mInstance;
+   }
+
+   public void createVisionMeasurement(RobotContainer container) {
+      container.getDrivebase().createVisionMeasurement();
+   }
+
 }
